6e3732311bb9695d4ff4ddaf013421d0b1b9e8f9
somekind12ofrandom <ttundras56@gmail.com>
2024-10-05 18:46:08 -0400

    tester.lua

--- tester.lua
+++ tester.lua
@@ -1,16 +1,7 @@
-if game.GameId == 10449761463
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/TSB.lua",True))()
- 
+if game.GameId == 3808081382 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/main/main/TSB.lua"))()
 else
- 
-if game.GameId == 12411473842 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/New-C4-Remote.lua/main/pressure_kuy.lua",True))()
- 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/crumies/Pressure-v2/main/script.lua",True))()
- 
-else
-        
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
- 
-game:GetService("StarterGui"):SetCore("SendNotification"), {Title = "(Script)", Text = "This game is not supported, Please wait while the universal script starts..", Duration = 5})
- end
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+
+  game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Tundra loader", Text = "This game is not supported, Please wait while the universal script executes..", Duration = 15})
+end
